3fb9468b39910ea1e1e3cca197161b7e96f686df
Your Name <eman_the_man@hotmail.com>
2017-06-15 23:50:39 -0600

    whoops forgot one

---
+++ models/IPortfolioInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PortfolioApi.Models
+{
+    interface IPortfolioInfo<T>
+    {
+        void Update(T model);
+    }
+}
