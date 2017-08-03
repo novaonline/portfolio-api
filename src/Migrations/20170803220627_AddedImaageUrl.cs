b3ed0f835ed328af4cf42c0bfb420cf9c4635ebb
Your Name <eman_the_man@hotmail.com>
2017-08-03 16:21:10 -0600

    added image url to framework

---
+++ src/Migrations/20170803220627_AddedImaageUrl.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace PortfolioApi.Migrations
+{
+    public partial class AddedImaageUrl : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "pfm_rankableitems_frameworks_info",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "pfm_profiles_info",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "pfm_profiles_info",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50,
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "pfm_rankableitems_frameworks_info");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "pfm_profiles_info",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "pfm_profiles_info",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
